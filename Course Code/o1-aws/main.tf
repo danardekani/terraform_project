364806be39929363852caac59e68565eeba78467
Daniel Ardekani <ardekanicapital@gmail.com>
2025-04-18 11:53:14 -0400

    create resource block for aws vpc

--- Course Code/o1-aws/main.tf
+++ Course Code/o1-aws/main.tf
@@ -14,3 +14,11 @@ provider "aws" {
   profile = "security-research"
 
 }
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    "Name" = "Practing Terraform Deployments"
+  }
+}
